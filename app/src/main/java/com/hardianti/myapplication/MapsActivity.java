5477489a33f42e1173431651a21599b424a9dcf4
hardiantii <81626509+hardiantii@users.noreply.github.com>
2021-05-07 20:54:20 +0800

    Update MapsActivity.java

--- app/src/main/java/com/hardianti/myapplication/MapsActivity.java
+++ app/src/main/java/com/hardianti/myapplication/MapsActivity.java
@@ -46,66 +46,76 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Untad1 and move the camera
-        LatLng rumah = new LatLng(-0.905528, 119.895435);
-        LatLng apotik_garuda = new LatLng(-0.90634, 119.89849);
-        LatLng apotik_kimia = new LatLng(-0.8984905, 119.8945723);
-        LatLng apotik_Sehat_Sentosa = new LatLng(-0.9191181, 119.8951155);
-        LatLng apotik_Diva_Farma = new LatLng(-0.8966845, 119.8816410);
-        LatLng apotik_Pelangi = new LatLng(-0.9187098, 119.8896434);
+        LatLng Home = new LatLng(-0.8436239, 119.8976752);
+        LatLng ATM_BNI_1 = new LatLng(-0.8360934, 119.8903794);
+        LatLng ATM_BNI_2 = new LatLng(-0.8356922, 119.8954361);
+        LatLng ATM_BNI_3 = new LatLng(-0.8581360, 119.8834774);
+        LatLng ATM_BNI_4 = new LatLng(-0.8743870, 119.8746030);
+        LatLng ATM_BNI_5 = new LatLng(-0.8886600, 119.8781525);
         //custom size marker
         int tinggi = 100;
         int lebar = 100;
-        BitmapDrawable bitmapStart = (BitmapDrawable) getResources().getDrawable(R.drawable.rumah);
-        BitmapDrawable bitmapDes = (BitmapDrawable) getResources().getDrawable(R.drawable.apotik1);
+        BitmapDrawable bitmapStart = (BitmapDrawable) getResources().getDrawable(R.drawable.pin_rumah_biru);
+        BitmapDrawable bitmapDes = (BitmapDrawable) getResources().getDrawable(R.drawable.pin_atm_biru);
         Bitmap s = bitmapStart.getBitmap();
         Bitmap d = bitmapDes.getBitmap();
         Bitmap MarkerStart = Bitmap.createScaledBitmap(s, lebar, tinggi, false);
         Bitmap MarkerDes = Bitmap.createScaledBitmap(d, lebar, tinggi, false);
 
         //add market to map
-        mMap.addMarker(new MarkerOptions().position(rumah).title("Rumah")
+        mMap.addMarker(new MarkerOptions().position(Home).title("Marker in My Home")
                 .snippet("Ini adalah Rumahku")
                 .icon(BitmapDescriptorFactory.fromBitmap(MarkerStart)));
 
-        mMap.addMarker(new MarkerOptions().position(apotik_garuda).title("Apotik Garuda")
-        .snippet("Ini adalah Apotik Garuda")
-        .icon(BitmapDescriptorFactory.fromBitmap(MarkerDes)));
-
-        mMap.addMarker(new MarkerOptions().position(apotik_kimia).title("Apotik Kimia Farma")
-                .snippet("Ini adalah Apotik Kimia Farma")
+        mMap.addMarker(new MarkerOptions().position(ATM_BNI_1).title("Marker in ATM BNI UNTAD 1")
+                .snippet("Ini adalah ATM BNI UNTAD 1")
                 .icon(BitmapDescriptorFactory.fromBitmap(MarkerDes)));
 
-        mMap.addMarker(new MarkerOptions().position(apotik_Sehat_Sentosa).title("Apotik Sehat Sentosa")
-                .snippet("Ini adalah Apotik Sehat Sentosa")
+        mMap.addMarker(new MarkerOptions().position(ATM_BNI_2).title("Marker in ATM BNI UNTAD 2")
+                .snippet("Ini adalah ATM BNI UNTAD 2")
                 .icon(BitmapDescriptorFactory.fromBitmap(MarkerDes)));
 
-        mMap.addMarker(new MarkerOptions().position(apotik_Diva_Farma).title("Apotik Diva Farma")
-                .snippet("Ini adalah Apotik Diva Farma")
+        mMap.addMarker(new MarkerOptions().position(ATM_BNI_3).title("Marker in ATM BNI UNDATA")
+                .snippet("Ini adalah ATM BNI UNDATA")
                 .icon(BitmapDescriptorFactory.fromBitmap(MarkerDes)));
 
-        mMap.addMarker(new MarkerOptions().position(apotik_Pelangi).title("Apotik Pelangi")
-                .snippet("Ini adalah Apotik Pelangi")
+        mMap.addMarker(new MarkerOptions().position(ATM_BNI_4).title("Marker in ATM BNI SPBU Yos Sudarso")
+                .snippet("Ini adalah ATM BNI SPBU Yos Sudarso")
+                .icon(BitmapDescriptorFactory.fromBitmap(MarkerDes)));
+
+        mMap.addMarker(new MarkerOptions().position(ATM_BNI_5).title("Marker in ATM BNI BNS S.Parman")
+                .snippet("Ini adalah ATM BNI BNS S.Parman")
                 .icon(BitmapDescriptorFactory.fromBitmap(MarkerDes)));
 
         mMap.addPolyline(new PolylineOptions().add(
-                rumah,
-                new LatLng(-0.905528, 119.895435),
-                new LatLng(-0.905611, 119.895409),
-                new LatLng(-0.905570, 119.895635),
-                new LatLng(-0.904994, 119.895576),
-                new LatLng(-0.904844, 119.896186),
-                new LatLng(-0.904633, 119.896977),
-                new LatLng(-0.904491, 119.897608),
-                new LatLng(-0.904368, 119.898188),
-                new LatLng(-0.905199, 119.898292),
-                new LatLng(-0.906245, 119.898389),
-                new LatLng(-0.906351, 119.898399),
-                new LatLng(-0.90634, 119.89849),
-                apotik_garuda
+                Home,
+                new LatLng(-0.8435212, 119.8976736),
+                new LatLng(-0.8435055, 119.8961028),
+                new LatLng(-0.8435038, 119.8957012),
+                new LatLng(-0.8434800, 119.8914412),
+                new LatLng(-0.8434589, 119.8908528),
+                new LatLng(-0.8425263, 119.8905835),
+                new LatLng(-0.8409691, 119.8902513),
+                new LatLng(-0.8404806, 119.8901678),
+                new LatLng(-0.8369039, 119.8894949),
+                new LatLng(-0.8363920, 119.8893762),
+                new LatLng(-0.8363511, 119.8893232),
+                new LatLng(-0.8363079, 119.8892924),
+                new LatLng(-0.8362434, 119.8892778),
+                new LatLng(-0.8361993, 119.8892806),
+                new LatLng(-0.8361603, 119.8893011),
+                new LatLng(-0.8361245, 119.8893293),
+                new LatLng(-0.8360967, 119.8893806),
+                new LatLng(-0.8360860, 119.8894463),
+                new LatLng(-0.8360965, 119.8894817),
+                new LatLng(-0.8361201, 119.8895415),
+                new LatLng(-0.8361714, 119.8902654),
+                new LatLng(-0.8361794, 119.8903700),
+                ATM_BNI_1
                 ).width(10)
                 .color(Color.BLUE)
         );
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(apotik_garuda, 14.5f));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ATM_BNI_1, 14.5f));
     }
 }
